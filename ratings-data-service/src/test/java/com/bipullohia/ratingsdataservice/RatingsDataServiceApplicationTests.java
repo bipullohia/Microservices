6b9b86bb7a4b91e88ff053fef9201436a8cef0f9
bipullohia <bipul.lohia@gmail.com>
2019-12-01 00:57:09 +0530

    initial commit

---
+++ ratings-data-service/src/test/java/com/bipullohia/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bipullohia.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
